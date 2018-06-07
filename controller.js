ca0e707b554f2e2be7e3d0c7b92a31384b24bf87
Calvin <calvcastle666@gmail.com>
2018-06-07 02:01:26 -0500

    Sistema de comentarios en html

--- controller.js
+++ controller.js
@@ -1,4 +1,19 @@
 var app = angular.module("Mi-primer-APP-AngularJS", [])
 app.controller("nombreControlador", function($scope){
- $scope.frase = " Un saludo...";
+ $scope.frase = "Agregar comentarios";
+ $scope.nuevoComentario = {};
+ $scope.comentarios = [
+ 	{
+ 		comentario: "Hola mundo todo bien !",
+ 		usuario: "Isma"
+ 	},
+ 	{
+ 		comentario: "Hola mundo todos locos !",
+ 		usuario: "Ivan"
+ 	}
+ ];
+ $scope.agregarComentario = function () {
+ 	$scope.comentarios.push($scope.nuevoComentario);
+ 	$scope.nuevoComentario = {};
+ }
 });﻿
